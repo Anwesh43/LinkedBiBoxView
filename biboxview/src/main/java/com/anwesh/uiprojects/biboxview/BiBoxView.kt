77f0080274e1d3230ed0ee04f37fd60b3bc1ec12
Anwesh43 <anweshthecool0@gmail.com>
2019-04-10 10:23:55 +0530

    adding BiBox class

--- biboxview/src/main/java/com/anwesh/uiprojects/biboxview/BiBoxView.kt
+++ biboxview/src/main/java/com/anwesh/uiprojects/biboxview/BiBoxView.kt
@@ -169,4 +169,27 @@ class BiBoxView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiBox(var i : Int) {
+
+        private var curr : BBNode = BBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
