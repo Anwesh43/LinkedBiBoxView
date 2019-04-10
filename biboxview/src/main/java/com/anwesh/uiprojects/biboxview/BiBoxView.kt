de7632ee266b2a9954ecbd68803ba446b2e5fd77
Anwesh43 <anweshthecool0@gmail.com>
2019-04-10 10:15:24 +0530

    adding BBNode class

--- biboxview/src/main/java/com/anwesh/uiprojects/biboxview/BiBoxView.kt
+++ biboxview/src/main/java/com/anwesh/uiprojects/biboxview/BiBoxView.kt
@@ -125,4 +125,48 @@ class BiBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBNode(var i : Int, val state : State = State()) {
+
+        private var next : BBNode? = null
+        private var prev : BBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBNode {
+            var curr : BBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
